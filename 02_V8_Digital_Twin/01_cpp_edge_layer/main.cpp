a3ee4d609dbbf3500924a10cda02a81b57c12504
DEBNEEL <debneelghosh26@example.com>
2026-09-11 23:46:40 +0530

    Feature: Implemented primitive double scalar math and cmath sine wave execution

--- 02_V8_Digital_Twin/01_cpp_edge_layer/main.cpp
+++ 02_V8_Digital_Twin/01_cpp_edge_layer/main.cpp
@@ -1,10 +1,17 @@
  #include <iostream>
+ #include <cmath>
 
 int main() {
     std::cout << "========================================" << std::endl;
     std::cout << "  V8 CYBER-PHYSICAL ENGINE SIMULATOR    " << std::endl;
     std::cout << "  STATUS: LOGICAL CORE ECU INITIALIZED  " << std::endl;
     std::cout << "========================================" << std::endl;
+
+    double t= 1.5;
+    double omega=0.5;
+    double ans = std::sin(t*omega);
+    std::cout << "The calculated answer is:"<< ans << std::endl;
+
     return 0;
 }
 
