5a5dc3714df3240ed142f08bf6bfb1eeb7622bc9
DEBNEEL <debneelghosh26@gmail.com>
2026-09-15 10:36:42 +0530

    Deliverable: Completed Week 1 Part D hello-engine array simulation via pointer arithmetic

--- 02_V8_Digital_Twin/01_cpp_edge_layer/main.cpp
+++ 02_V8_Digital_Twin/01_cpp_edge_layer/main.cpp
@@ -1,6 +1,12 @@
  #include <iostream>
  #include <cmath>
 
+ void calc(double *ptr, int length,double omega, double dt) {
+    for (int i = 0; i < length ; i++){
+        *(ptr+i) = std::sin(omega*(i*dt));
+    }
+ }
+
 int main() {
     std::cout << "========================================" << std::endl;
     std::cout << "  V8 CYBER-PHYSICAL ENGINE SIMULATOR    " << std::endl;
@@ -8,6 +14,17 @@ int main() {
     std::cout << "========================================" << std::endl;
 
     std::cout << "Lets TEST SOMETHING"<< " " << "Some Bullshit Hard Coded Value" << std::endl;
+    double rpm = 3000;
+    double srate = 1000;
+    long double pi = 3.141592653589793;
+    double omega = (rpm/60)*2*pi;
+    double dt = 1/srate;
+    double values[10]={};
+    calc (values,10, omega, dt);
+    for (int i = 0; i<10; i++){
+        std::cout << "Sample" << i << ":" << *(values+i) << std::endl;
+    }
+
 
     return 0;
 }
